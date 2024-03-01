b9365ede353b1de562cf76d38cb8421d9f1846b8
50059694 <50059695@belfastmet.ac.uk>
2024-03-01 14:52:44 +0000

    week 1

---
+++ WEEK-1/learningStrings.py
@@ -0,0 +1,27 @@
+my_string1="Hello World"
+my_string2="Python is awesome"
+my_string3="I can use special characters like \n to create new lines"
+
+print(my_string3)
+
+varNow=my_string1
+print(varNow[3])
+print(varNow[-1])
+print(varNow[3:5])
+print(varNow[4:10])
+
+varNow=my_string2
+print(varNow[3])
+print(varNow[-1])
+print(varNow[3:5])
+print(varNow[4:10])
+
+varNow=my_string3
+print(varNow[3])
+print(varNow[-1])
+print(varNow[3:5])
+print(varNow[4:10])
+
+print(" ")
+print(" ")
+
---
+++ WEEK-1/stringComparison.py
@@ -0,0 +1,41 @@
+name="Daniel Smith"
+userInput=input("What is your name?")
+
+if userInput==name:
+    print("Correct")
+else:
+    print("Incorrect")
+
+print()
+print()
+
+if "Da" in userInput:
+    print("Da is in the input")
+else:
+    print("Da is not in the input")
+print()
+if "ma" in userInput:
+    print("ma is in the input")
+else:
+    print("ma is not in the input")
+print()
+if "en" in userInput:
+    print("en is in the input")
+else:
+    print("en is not in the input")
+print()
+if "a" in userInput:
+    print("a is in the input")
+else:
+    print("a is not in the input")
+print()
+if "e" in userInput:
+    print("e is in the input")
+else:
+    print("e is not in the input")
+print()
+if "i" in userInput:
+    print("i is in the input")
+else:
+    print("i is not in the input")
+print()
